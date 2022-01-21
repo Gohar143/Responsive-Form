9a9625a64f5d1bd941805d3b0089394b806a6c39
Gohar Abbas Anjum <87871703+Gohar143@users.noreply.github.com>
2022-01-21 14:08:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//focus
+function focusFunction(element){
+    element.style.background = "#0d6efd";
+    element.style.color = "white";
+}
+//blur
+function blurFunction(element){
+    element.style.background = "";
+    element.style.color = "";
+}
